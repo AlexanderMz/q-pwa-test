700909066c860f5beba4444975878776c231f592
Alexa Martinez Tilbe <AlexanderMz@users.noreply.github.com>
2025-01-23 19:47:06 -0500

    ...

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,11 @@
 
 import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { firebaseConfig, vapidKey } from './config';
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "push-notification-cb084.firebaseapp.com",
-  projectId: "push-notification-cb084",
-  storageBucket: "push-notification-cb084.firebasestorage.app",
-  messagingSenderId: "811825584003",
-  appId: "1:811825584003:web:973e20241669cb57d808b6",
-  measurementId: "G-J50FCL38Y8"
-};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
@@ -33,7 +23,7 @@ onMessage(messaging, (payload) => {
   console.log('Message received. ', payload);
   // ...
 });
-getToken(messaging, { vapidKey: 'BIrbkokeyGGkQa59lWw8DRLIQ_SoNw0RJckC7ilDq6ehZkI7Sl3EdFMtuXlod6zvWrgqfNWyB-iu_IEzmXZfTYw' }).then((currentToken) => {
+getToken(messaging, { vapidKey  }).then((currentToken) => {
   if (currentToken) {
     // Send the token to your server and update the UI if necessary
     console.log('Token is:', currentToken)
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "push-notification-cb084.firebaseapp.com",
+  projectId: "push-notification-cb084",
+  storageBucket: "push-notification-cb084.firebasestorage.app",
+  messagingSenderId: "811825584003",
+  appId: "1:811825584003:web:973e20241669cb57d808b6",
+  measurementId: "G-J50FCL38Y8"
+};
+
+export const vapidKey = 'BIrbkokeyGGkQa59lWw8DRLIQ_SoNw0RJckC7ilDq6ehZkI7Sl3EdFMtuXlod6zvWrgqfNWyB-iu_IEzmXZfTYw'
