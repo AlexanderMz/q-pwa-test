cd4452a6669e2abe554180791b4862a0aaed4f82
Alexa Martinez Tilbe <AlexanderMz@users.noreply.github.com>
2025-01-23 20:13:40 -0500

    ...

--- src/App.vue
+++ src/App.vue
@@ -36,4 +36,15 @@ getToken(messaging, { vapidKey  }).then((currentToken) => {
   console.log('An error occurred while retrieving token. ', err);
   // ...
 });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./firebase-messaging-sw.js")
+    .then(function(registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function(err) {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
+
 </script>
