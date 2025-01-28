4e5dc2696956205e04ae2aad383e32af2576b602
Alexa Martinez Tilbe <AlexanderMz@users.noreply.github.com>
2025-01-27 20:54:37 -0500

    -.-.-

--- src-pwa/custom-service-worker.ts
+++ src-pwa/custom-service-worker.ts
@@ -33,3 +33,34 @@ if (process.env.MODE !== 'ssr' || process.env.PROD) {
     )
   );
 }
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js')
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+})
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage(async (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload)
+  // Customize notification here
+  const notificationTitle = payload.notification.title
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png',
+  }
+
+  self.registration.showNotification(notificationTitle, notificationOptions)
+})
